088e79c8bdcd29289941fef26c14f10a07bda90b
dimitriosvalogiorgis <dimitriosvalogiorgis@gmail.com>
2025-02-26 16:38:50 +0200

    routing paths

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
+import { TuiRoot } from '@taiga-ui/core';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    TuiRoot,
+    HomeComponent,
+    AboutComponent,
+    ProjectsComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'CV-Website';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home Page',
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+    title: 'About',
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent,
+    title: 'Projects',
+  },
+];
