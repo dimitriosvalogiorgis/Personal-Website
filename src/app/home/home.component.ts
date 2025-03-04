b7db203efba86f258f21d30961d0cbb4618d9ee0
dimitriosvalogiorgis <dimitriosvalogiorgis@gmail.com>
2025-03-04 14:48:12 +0200

    css for buttons, aligning

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { TuiButton } from '@taiga-ui/core';
+import { TuiButton, TuiIcon, TuiOption } from '@taiga-ui/core';
+import { TuiAvatar } from '@taiga-ui/kit';
+
 @Component({
   standalone: true,
   selector: 'app-home',
-  imports: [TuiButton],
+  imports: [TuiButton, TuiAvatar, TuiIcon, TuiOption],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
