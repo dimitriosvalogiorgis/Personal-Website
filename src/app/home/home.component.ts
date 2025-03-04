6275575a5ce912f2ab3b07aa6392baa4564cae53
dimitriosvalogiorgis <dimitriosvalogiorgis@gmail.com>
2025-03-04 16:51:52 +0200

    footer added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
-import { TuiButton, TuiIcon, TuiOption } from '@taiga-ui/core';
+import { TuiButton, TuiIcon, TuiOption, TuiGroup } from '@taiga-ui/core';
 import { TuiAvatar } from '@taiga-ui/kit';
-
+import { TuiButtonGroup, TuiIconBadge } from '@taiga-ui/kit';
 @Component({
   standalone: true,
   selector: 'app-home',
-  imports: [TuiButton, TuiAvatar, TuiIcon, TuiOption],
+  imports: [
+    TuiButton,
+    TuiAvatar,
+    TuiIcon,
+    TuiOption,
+    TuiGroup,
+    TuiButtonGroup,
+    TuiIconBadge,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
