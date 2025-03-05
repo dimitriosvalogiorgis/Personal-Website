3f2e4f29446982de65974129f038b7da2c3b627b
dimitriosvalogiorgis <dimitriosvalogiorgis@gmail.com>
2025-03-05 13:51:35 +0200

    fixed img pathing, carousel done

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { TuiCarousel } from '@taiga-ui/kit';
 
 @Component({
   standalone: true,
   selector: 'app-projects',
-  imports: [],
+  imports: [TuiCarousel],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProjectsComponent {}
+export class ProjectsComponent {
+  index = 0;
+  readonly voting = ['1.png', '2.png', '3.png'];
+  musicIndex = 0;
+  readonly music = ['1.png', '2.png', '3.png', '4.png', '5.png'];
+  azxIndex = 0;
+  readonly azx = ['1.png', '2.png', '3.png', '4.png'];
+}
